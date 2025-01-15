dfab0422814f44c063505198ad79de511d6f4662
1srah <s6603052413141@email.kmutnb.ac.th>
2025-01-15 15:12:37 +0700

    add ref-func.js
    add ref-func.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { EvenData2 } from './event-data';
+import RefsFunc from './refs-func';
 
 function App() {
-  return ( <EvenData2/>
+  return ( <RefsFunc/>
   );
 }
 
